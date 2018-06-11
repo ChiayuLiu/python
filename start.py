48a8a6dbd78c41e2dd59866fbdfe5114243959ac
ChiayuLiu <36464089+ChiayuLiu@users.noreply.github.com>
2018-06-11 22:14:35 +0800

    Add files via upload

---
+++ start.py
@@ -0,0 +1,65 @@
+# #這是註解哦
+# #數字
+# 3
+# 3.5
+# #print(資料)
+# print(3.5)
+# #字串
+# "任意門"
+# print("ㄤㄤㄤ")
+# #布林值
+# True
+# False
+# print(False)
+# #變數
+# input=3
+# print(input)
+# input="Hello world"
+# print(input)
+# #運算符號
+# data=3+5
+# print(data)
+# #整數除法
+# data=5//2
+# print(data)
+# #乘法
+# data=6*2
+# print(data)
+# #次方
+# data=6**3
+# print(data)
+# print("hello"+"world")
+#提示文字/要求使用者輸入
+# input("Enter a Number:")
+# n1=input("Enter a Number:") #接到變數 #以字串的型態取得使用者輸入
+# n2=input("Enter a Number:")
+# #int(資料)將資料轉換成整數型態
+# n1=int(n1)#把原來的字串改成數字
+# n2=int(n2)
+# result=n1+n2
+# print(result)
+#讓使用者輸入五個數字，然後1.告訴人家哪一個數字最大2.算總額
+#題目二
+n1=input("Enter a Number:") 
+n2=input("Enter a Number:")
+n3=input("Enter a Number:") 
+n4=input("Enter a Number:")
+n5=input("Enter a Number:") 
+n1=int(n1)
+n2=int(n2)
+n3=int(n3)
+n4=int(n4)
+n5=int(n5)
+result=n1+n2+n3+n4+n5
+print(result)
+#題目一
+n1=input("Enter a Number:") 
+n2=input("Enter a Number:")
+n3=input("Enter a Number:") 
+n4=input("Enter a Number:")
+n5=input("Enter a Number:") 
+n1=int(n1)
+n2=int(n2)
+n3=int(n3)
+n4=int(n4)
+n5=int(n5)
