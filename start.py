a54f0c8e5869ba50e59e9a5562fa2a6fa123b9ab
ChiayuLiu <36464089+ChiayuLiu@users.noreply.github.com>
2018-06-12 11:07:01 +0800

    Add files via upload

--- start.py
+++ start.py
@@ -38,7 +38,25 @@
 # n2=int(n2)
 # result=n1+n2
 # print(result)
+
+#HW1
 #讓使用者輸入五個數字，然後1.告訴人家哪一個數字最大2.算總額
+
+#題目一
+n1=input("Enter a Number:") 
+n2=input("Enter a Number:")
+n3=input("Enter a Number:") 
+n4=input("Enter a Number:")
+n5=input("Enter a Number:") 
+n1=int(n1)
+n2=int(n2)
+n3=int(n3)
+n4=int(n4)
+n5=int(n5)
+nums=[n1,n2,n3,n4,n5]
+max_nums=max(nums)
+print(max_nums)
+
 #題目二
 n1=input("Enter a Number:") 
 n2=input("Enter a Number:")
@@ -52,14 +70,3 @@ n4=int(n4)
 n5=int(n5)
 result=n1+n2+n3+n4+n5
 print(result)
-#題目一
-n1=input("Enter a Number:") 
-n2=input("Enter a Number:")
-n3=input("Enter a Number:") 
-n4=input("Enter a Number:")
-n5=input("Enter a Number:") 
-n1=int(n1)
-n2=int(n2)
-n3=int(n3)
-n4=int(n4)
-n5=int(n5)
